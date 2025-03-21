d002303c919a6af2792fbe985160c16548335342
Abhishek Pandit <abhishekpandit986@gmail.com>
2025-03-21 07:42:25 +0000

    homepage

---
+++ webpage/src/App.jsx
@@ -0,0 +1,11 @@
+import HomePage from "./pages/homepage/HomePage"
+
+function App() {
+  return (
+    <>
+      <HomePage />
+    </>
+  )
+}
+
+export default App
---
+++ webpage/src/pages/homepage/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from "./styles/HomePage.module.css";
+
+const HomePage = () => {
+    return (
+        <div className={styles.homepageContainer}>
+            <div>Hello</div>
+        </div>
+    )
+}
+
+export default HomePage
